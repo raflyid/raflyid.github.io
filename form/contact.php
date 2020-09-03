95a49d860035732ce808cf6f3f5991b185246ae9
raflyid <raflyid@hotmail.com>
2020-09-03 19:18:09 +0700

    fixed get in touch

--- form/contact.php
+++ form/contact.php
@@ -4,13 +4,15 @@
  */
 
 // an email address that will be in the From field of the email.
-$from = 'Demo contact form <YOUR_EMAIL@DOMAIN.COM>';
+$from = $_POST['InputEmail'];
 
 // an email address that will receive the email with the output of the form
-$sendTo = 'Demo contact form <YOUR_EMAIL@DOMAIN.COM>';
+$sendTo = 'raflyid@hotmail.com';
 
 // subject of the email
-$subject = 'New message from contact form';
+$subject = $_POST['InputSubject'];
+
+$message = $_POST['InputMessage'];
 
 // form field names and their translations.
 // array variable name => Text to appear in the email
